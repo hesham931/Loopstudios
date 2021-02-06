3ff42b4e7e449f4eb2c7aa52069132f75cd8cc79
hesham931 <69353244+hesham931@users.noreply.github.com>
2021-02-07 01:28:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,10 @@ window.addEventListener('scroll',() => {
     else{
         document.getElementById("nav").style="background-color: transparent;";
     }
+    if(scrolled > 160)
+        document.getElementById("body_middle").style="animation-name: A;animation-delay: 0s;animation-duration: 5s;animation-iteration-count: 1;";
+    if(scrolled > 630)
+        document.getElementById("end").style="animation-name: B;animation-delay: 0s;animation-duration: 5s;animation-iteration-count: 1;";
+    if(scrolled > 1800)
+        document.getElementById("information").style="animation-name: C;animation-delay: 0s;animation-duration: 5s;animation-iteration-count: 1;";
     });
